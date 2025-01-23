ada36bbcc9f493d00b0251fd710362879ce0bae6
Jonhks <jonhgx@gmail.com>
2025-01-22 22:13:49 -0600

    Change class

--- vite.config.ts
+++ vite.config.ts
@@ -45,29 +45,3 @@ export default defineConfig({
     },
   },
 });
-
-// export default defineConfig({
-//   plugins: [
-//     VitePWA({
-//       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
-//       manifest: {
-//         name: 'My Awesome App',
-//         short_name: 'MyApp',
-//         description: 'My Awesome App description',
-//         theme_color: '#ffffff',
-//         icons: [
-//           {
-//             src: 'pwa-192x192.png',
-//             sizes: '192x192',
-//             type: 'image/png'
-//           },
-//           {
-//             src: 'pwa-512x512.png',
-//             sizes: '512x512',
-//             type: 'image/png'
-//           }
-//         ]
-//       }
-//     })
-//   ]
-// })
