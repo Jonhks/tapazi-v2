9874302788efead98b27b12a666524a4f93483f1
Jonhks <jonhgx@gmail.com>
2025-07-18 22:38:51 -0600

    cambio a wip

--- src/views/WIP/WIP.tsx
+++ src/views/WIP/WIP.tsx
@@ -2,7 +2,7 @@ const WIP = () => {
   return (
     <div>
       <img
-        src="/src/assets/img/construccion.jpg"
+        src="../construccion.jpg"
         alt="WIP"
         width={"100%"}
         style={{ height: "100vh", objectFit: "cover" }}
