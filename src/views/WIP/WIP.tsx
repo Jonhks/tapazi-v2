334b05170ad1d2b6cc39d9dd501336051d29afb2
Jonhks <jonhgx@gmail.com>
2025-07-28 20:04:36 -0600

    Update/v2.2

--- src/views/WIP/WIP.tsx
+++ src/views/WIP/WIP.tsx
@@ -8,6 +8,8 @@ const WIP = () => {
       ? "https://s3.mx-central-1.amazonaws.com/portfolio.pool/resources/coming_soon_rose.png?quality=80&format=webp"
       : "https://s3.mx-central-1.amazonaws.com/portfolio.pool/resources/coming_soon_orange.png?quality=80&format=webp";
 
+  // cometarios innecesarios
+
   return (
     <div>
       <img
