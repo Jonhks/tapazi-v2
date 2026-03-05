74a2915fb81495b1708913ec2e4b4bf78a3be2c1
Jonhks <jonhgx@gmail.com>
2026-03-04 19:33:07 -0600

    Update/ probando para deploy en vercel

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const url = "https://portfolio-pool-test.damnserver.com:443"
+const url = "https://portfolio-pool-test.damnserver.com:443";
+// no se pueden subir ya cambios a main probando deploy en vercel de dev
 
 const apiEnv = axios.create({
   baseURL: url,
